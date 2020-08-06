778e2a678897bb69282aca9b987353d5ddd48bd5
ferror18 <ferror18@gmail.com>
2020-08-06 15:26:18 -0700

    mvp both days

--- src/player.py
+++ src/player.py
@@ -1,2 +1,39 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+directionMap = { 'n': 'north', 's':'south', 'e': 'east', 'w': 'west'}
+class Player:
+    def __init__(self, room, name="chicken little", keys=[], win=False):
+        self.name = name
+        self.room = room
+        self.keys = keys
+        self.win = win
+    def countKeys(self):
+        print(f'Congrats {self.name}, you\'ve found this keys:')
+        for i in self.keys:
+            print(i)
+        print(f'Total: {len(self.keys)} out of 1')
+    def __str__(self):
+        return f'''\n\tName: {self.name}\n\tRoom: {self.room}\n\tKeys: {self.keys} \n'''
+    def getItem(self):
+            self.keys.append(self.room.keys.pop(self.room.keys.index('golden')))
+    def dropItem(self):
+            self.room.keys.append(self.keys.pop(self.keys.index('golden')))
+    def move(self, direction):
+            # print('direction:', direction, 'golden' in self.keys )
+            print(f'\n\n\n You\'ve choosen to go {directionMap.get(direction)}...\n')
+            nextRoom = getattr(self.room, f'{direction}_to', None)
+            if nextRoom is None:
+                print(f'There is nothing in this direction, so you go back...')
+            elif nextRoom.name == 'treasure':
+                if 'golden' in self.keys:
+                    self.room = nextRoom
+                    print(f'\t\nYou find yourself {self.room.title}, you use the key to open it')
+                    print(f'\t\n{self.room.description}')
+                    print(f'\t\nEnd of the game for our hero {self.name}')
+                    self.win = True
+                else:
+                    print(f'You find a massive vault door, it\'s locked and it seems to need a key')
+                    print(f'to open, you go back trying to search for the key.\n')
+            else:
+                self.room = nextRoom
+                    
--- src/room.py
+++ src/room.py
@@ -1,2 +1,21 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+x = "Theres nothing is this direction"
+class Room:
+    def __init__(self, name, title, description, keys=[]):
+        self.name = name
+        self.title = title
+        self.description = description
+        self.keys = keys
+    def __str__(self):
+        return f'''
+            \t\tName: {self.name}
+            \t\tTitle: {self.title}
+            \t\tDescription: {self.description}
+            \t\tKeys: {self.keys}'''
+    def searchRoom(self):
+        print(f'You\'ve searched in {self.name}...')
+        print(f'Congrats, you\'ve found this keys:')
+        for i in self.keys:
+            print(f' - {i} \n')
+        print(f'\nUse "get" or "drop" to get/drop these items\n\n')
